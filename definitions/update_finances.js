316821b35a9bd870b05b068cd2a276e17d79883d
Sudarshan <rsudarshanreddy1@gmail.com>
2024-12-23 19:13:53 +0000

    commit changes

---
+++ definitions/update_finances.js
@@ -0,0 +1,45 @@
+// publish("tbl_daily_fin_updates", {
+//     type: "incremental",
+//     schema: "dataform",
+//     tags: ["Forecast"]
+// }).query(`
+//   SELECT 
+//     Stock, 
+//     cmp, 
+//     previous_change, 
+//     _1_day_change
+//   FROM 
+//     \`sudarshan-442212.fin.finances\`
+// `);
+
+operate("update_finances_from_gsheets")
+    .tags("Forecast")
+    .queries(` MERGE INTO \`sudarshan-442212.dataform.tbl_daily_fin_updates\` AS Target
+      USING (
+        SELECT 
+          Stock,
+          cmp,
+          previous_change,
+          _1_day_change 
+        FROM \`sudarshan-442212.fin.finances\`
+      ) AS Source
+      ON Source.Stock = Target.Stock
+      WHEN MATCHED THEN
+        UPDATE SET 
+          Target.cmp = Source.cmp,
+          Target.previous_change = Source.previous_change,
+          Target._1_day_change = Source._1_day_change
+      WHEN NOT MATCHED THEN
+        INSERT (
+          Stock,
+          cmp,
+          previous_change,
+          _1_day_change
+        )
+        VALUES (
+          Source.Stock,
+          Source.cmp,
+          Source.previous_change,
+          Source._1_day_change              
+              )
+        `);
